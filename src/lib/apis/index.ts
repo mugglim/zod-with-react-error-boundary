388cc0931e3520f028f5a055ad7a56dd77740f30
jaeuk lim <applewodnr@naver.com>
2022-12-04 17:16:19 +0900

    feat: 스키마 에러를 강제할 수 있는 env 추가

--- src/lib/apis/index.ts
+++ src/lib/apis/index.ts
@@ -1,6 +1,10 @@
 import { TodoListSchema } from '../schemas/todo';
 
 const getTodoList = async () => {
+  if (import.meta.env.VITE_ENABLE_FETCH_ERROR === 'true') {
+    return TodoListSchema.parse([{ x: 1 }]);
+  }
+
   const response = await fetch(`${import.meta.env.VITE_BASE_URL}/todos`);
   const todoList = await response.json();
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_BASE_URL: string;
+  readonly VITE_ENABLE_FETCH_ERROR: string;
 }
